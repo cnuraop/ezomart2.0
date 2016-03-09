32afbb61016c4c8582a2d2baf29294d38efbf907
cnuraop <srikanth8605@gmail.com>
2016-03-09 18:15:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ezomart10
+1.0
+![first](https://cloud.githubusercontent.com/assets/7678255/13598290/0e4a2c8e-e543-11e5-96b8-d702c72371e8.png)
---
+++ app/src/main/java/info/androidhive/materialdesign/Fragments/ThreeFragment.java
@@ -0,0 +1,29 @@
+package info.androidhive.materialdesign.Fragments;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import info.androidhive.materialdesign.R;
+
+
+public class ThreeFragment extends Fragment {
+
+    public ThreeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+
+
+
+}
